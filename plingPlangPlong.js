4c5b3cc3d76b59b13c3d5ca275a8583420f4f02a
JawaherAb <jawaheralabdullah0@gmail.com>
2019-04-23 11:04:27 +0300

    great! I finished

---
+++ plingPlangPlong.js
@@ -0,0 +1,15 @@
+
+let num = 1755;
+ 
+if (num % 5 === 0 && num % 3 === 0){
+     console.log('PllingPlang')
+} else if ( num % 5 === 0 ) {
+     console.log('plang')
+} else if ( num % 7 === 0 ) {
+     console.log('plong')
+} else if ( num % 3 === 0 ) {
+    console.log('pling') 
+}
+else {
+    console.log(num)
+}
