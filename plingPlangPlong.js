c38abf43192caef5a476df92a0dde1c32c380085
samahab77 <s.bagido@gmail.com>
2019-04-23 11:13:07 +0300

    finsh warmup it was lettel confusing

---
+++ plingPlangPlong.js
@@ -0,0 +1,21 @@
+let num = 28 ;
+
+
+
+if (num%3 ===0 && num %5 ===0 && num %7 ===0){
+    console.log("plangplingplong");
+}
+else if (num%3 ===0 && num %5 ===0 ){
+    console.log("plangpling");
+}else if (num%5 ===0 && num%7===0 ){
+    console.log("plangplong");
+}
+else if (num % 3 ===0 ){
+    console.log("Pling");
+} else if (num %5 ===0){
+    console.log("Plang");
+} else if (num % 7===0){
+    console.log("Plong");
+}else {
+    console.log("string.")
+}
