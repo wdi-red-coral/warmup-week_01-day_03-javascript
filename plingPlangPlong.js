a94317627adc12caa7d86c66757c07222f66493f
saeedalzahrani <saeedalzahrani103@gmail.com>
2019-04-23 11:28:55 +0300

    ....

---
+++ plingPlangPlong.js
@@ -0,0 +1,20 @@
+let num = 20;
+
+        
+        
+        if(num%7===0){
+        console.log("Plong");
+        }
+        else if(num%5===0 && num%7 ===0){
+            console.log("Plang");
+            }
+        else if(num%3===0 && num%5===0){
+            console.log("plingplang");  
+            }
+        else if(num%3===0 && num%5===0 && num%7===0){
+                console.log("plingplangplog"); 
+            }
+         else if(num%3===0 && num%5===0 && num%7===0){
+                console.log(num); 
+        }
+
