63bd40ed268399c2aaacb4902c1f4e77b1bbb288
MahaKarban <maha.ahmad.karban@gmail.com>
2019-04-23 11:44:40 +0300

    add solution

--- plingPlangPlong.js
+++ plingPlangPlong.js
@@ -1,16 +1,16 @@
 let number=0
 n+=0
 
-if ((n%3 == 0);{      
+if ((n%3) === 0);{      
     console.log("pling")  
 }   
 
 
-else if ((n%5) == 0); {  
+else if ((n%5) === 0); {  
     console.log("plang")                
 }
 
-else if ((n%7) == 0); {                    
+else if ((n%7) === 0); {                    
     console.log("plong");                  
 }
 
