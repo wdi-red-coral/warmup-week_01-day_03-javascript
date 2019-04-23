c238aa1b50331f8acb84285f7a44fa3f7837612d
Nader <na511dr@hotmail.com>
2019-04-23 11:03:44 +0300

    Add solution in a seperate js file

---
+++ PlingPlangPlong.js
@@ -0,0 +1,17 @@
+let num = 33;
+
+if (num % 3 == 0) { 
+    console.log('Pling');
+
+} 
+else if (num % 5 == 0) { 
+    console.log('Plang');
+
+} 
+else if (num % 7 == 0) { 
+    console.log('Plong');
+
+} 
+else { 
+    console.log(num); 
+}
