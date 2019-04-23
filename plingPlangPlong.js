0adcb384f21eaed09c57aabd94fa251a5bd7ab11
ahmedalthagafi <a.althagafi92@gmail.com>
2019-04-23 11:26:03 +0300

    plingPlangPlong

---
+++ plingPlangPlong.js
@@ -0,0 +1,11 @@
+let num = 53;
+let tex = toString(num);
+if(num % 7 === 0 ){
+console.log("pling");
+}else if (num % 3 === 0 ){
+console.log("plang");
+}else if (num % 5 === 0){
+console.log("plong");
+}else {
+console.log(tex);
+}
