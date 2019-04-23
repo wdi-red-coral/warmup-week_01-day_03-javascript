b544e5e1db2a267324d0f0b03f26e0b41cef6eb1
samahab77 <s.bagido@gmail.com>
2019-04-23 11:27:49 +0300

    some change added

--- plingPlangPlong.js
+++ plingPlangPlong.js
@@ -9,7 +9,8 @@ else if (num%3 ===0 && num %5 ===0 ){
     console.log("plangpling");
 }else if (num%5 ===0 && num%7===0 ){
     console.log("plangplong");
-}
+}else if (num%3===0 && num%7===0 ){
+    console.log("plangplong");}
 else if (num % 3 ===0 ){
     console.log("Pling");
 } else if (num %5 ===0){
