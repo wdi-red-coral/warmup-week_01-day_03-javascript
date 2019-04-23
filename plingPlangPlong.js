6f6370a20ec22a7d3a2f34a15310dbf7d5f13fd4
omar4 <tarawri.dev@gmail.com>
2019-04-23 11:27:41 +0300

    good warmup exit

---
+++ plingPlangPlong.js
@@ -0,0 +1,30 @@
+let num1 = 8;
+let index = 0;
+
+if (num1 % 3 === 0)
+{
+    console.log('Pling');
+    index++;
+}
+
+if (num1 % 5 === 0)
+{
+ console.log('Pling');
+ index++;
+}
+
+if (num1 % 7 === 0)
+{
+ console.log('Pling');
+ index++;
+}
+
+ if(index === 0)
+    console.log(num1.toString());
+
+
+
+
+
+
+
